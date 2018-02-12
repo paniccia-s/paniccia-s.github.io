e4bba0730944ca34a442d41bf72f5eed8ba13159
Sam <paniccia.s@husky.neu.edu>
2018-02-12 11:32:22 -0500

    WIP Screen
    Added WIP screen, JS to toggle which wrap content is displayed.

--- js/panic.js
+++ js/panic.js
@@ -1,8 +1,21 @@
+//All Javascript by Sam Paniccia (so much code!). Using Google Materialize Design API. 
+
+
+//Should I show the work-in-progress screen, or should I show the real content? 
+var isBuild = true;
 
 $(document).ready(function(){
 	$('.dropdown-button').dropdown();
 	$('.dropdown-button-mobile').dropdown();
 	$('.button-collapse').sideNav({
 		edge: 'right'
-	});
+	}); 
+
+	//Determine which content to show/hide by isBuild flag
+	if (isBuild) { 
+		//in build mode, display wip content
+		document.getElementById("true-wrap").className = "hide";
+	} else { 
+		document.getElementById("wip-wrap").className = "hide";
+	}
 });
